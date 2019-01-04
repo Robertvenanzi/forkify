b6fa27b8cdb1110c08c78a0f13a7ee1c15104a95
robertvenanzi <robertgvenanzi@gmail.com>
2019-01-03 19:03:59 -0800

    imported key from config.js

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
+import { key } from '../config';
+
 
 export default class search {
     constructor(query) {
         this.query = query;
     }
     async getResults() {
-        const key = '83269883b402452f6077c0f190678bb6';
+
         try {
             const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
             this.result = res.data.recipes;
